eaca7a94cc8660948f306718694c6d5bdb6c3398
AlexPit <alexpit63@gmail.com>
2019-05-18 13:05:22 +0300

    захардкодил линк линк метода config/get

--- src/main/java/ru/sbrf/study/service/layers/BusinessService.java
+++ src/main/java/ru/sbrf/study/service/layers/BusinessService.java
@@ -159,7 +159,8 @@ public class BusinessService {
     private ConfigDTO getMaxSummParam(){
         KeyDTO keyDTO = new KeyDTO();
         keyDTO.setKey("maxSum");
-        return restTemplate.exchange(serviceLocator.apply(CONFIG_SERVICE_NAME) + "/get", HttpMethod.GET, new HttpEntity<>(keyDTO), new ParameterizedTypeReference<ConfigDTO>() {}).getBody();
+        //return restTemplate.exchange(serviceLocator.apply(CONFIG_SERVICE_NAME) + "/get", HttpMethod.GET, new HttpEntity<>(keyDTO), new ParameterizedTypeReference<ConfigDTO>() {}).getBody();
+        return restTemplate.exchange("http://e.n1ks.ru:32802/config/get", HttpMethod.GET, new HttpEntity<>(keyDTO), new ParameterizedTypeReference<ConfigDTO>() {}).getBody();
         //return new MaxSumm(Mock.getMaxSumm());
     }
 }
